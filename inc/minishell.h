290f068b751dfa25f58a6b1ffec785b56e495f42
Basil ALBADER <baalbade@student.42.fr>
2023-09-08 16:11:18 +0200

    minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -13,4 +13,21 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*
+# include "../TOOLS/libtools/libtools.h"
+# include "../TOOLS/ft_printf/ft_printf.h"
+# include "../TOOLS/minishelltools/minishelltools.h"
+*/
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <errno.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <stdbool.h>
+
 #endif // !MINISHELL_H
