1891ce44cc63fa3cdb53781fe6e0800e051f112e
Basil ALBADER <baalbade@student.42.fr>
2023-09-08 17:31:36 +0200

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
+int	main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	(void)env;
+	return (0);
+}
