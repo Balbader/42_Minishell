e372af1482fe5fa57763cabc2530c03321083946
Basil ALBADER <baalbade@student.42.fr>
2023-09-08 16:11:22 +0200

    structures.h.

--- inc/structures.h
+++ inc/structures.h
@@ -13,4 +13,52 @@
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
+enum	e_status
+{
+	SUCCESS = 0,
+	FAILURE = -1
+};
+
+enum	e_start
+{
+	START = 0,
+	END
+};
+
+typedef struct s_token
+{
+	char			*word;
+	int				type;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_exec
+{
+	char	**cmd;
+	char	*path;
+}	t_exec;
+
+typedef struct s_data
+{
+	char	*input;
+	t_token	*cmd_line;
+	char	**env;
+	bool	subgect_flag;
+}	t_data;
+
+/************************** basil stuff*/
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+/*
+function(t_list *list)
+{
+	t_env env = (t_env)list->content;
+}
+*/
+
 #endif // !STRUCTURES_H
