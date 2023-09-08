90ebd7d03ed0a5d68fec2306e9a50dce2e7b754e
Basil ALBADER <baalbade@student.42.fr>
2023-09-08 17:31:28 +0200

    env.c

--- srcs/builtins/env/env.c
+++ srcs/builtins/env/env.c
@@ -11,3 +11,24 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_exec_env(t_cmdlst *cmd, int fd_out, t_env *env)
+{
+	t_env	*tmp;
+
+	if (!cmd || !env)
+		return (ft_putstr_fd("bash: env: env not defined\n", 2),
+			EXIT_FAILURE);
+	tmp = env;
+	while (tmp)
+	{
+		if (tmp->value != NULL)
+		{
+			ft_putstr_fd(tmp->key, fd_out);
+			ft_putstr_fd("=", fd_out);
+			ft_putendl_fd(tmp->value, fd_out);
+		}
+		tmp = tmp->next;
+	}
+	return (EXIT_SUCCESS);
+}
