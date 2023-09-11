5224054ad3a3cc89f4c5441610055b0f79b7d4d3
Basil ALBADER <baalbade@student.42.fr>
2023-09-11 09:43:15 +0200

    WIP

--- srcs/main.c
+++ srcs/main.c
@@ -17,10 +17,16 @@ int main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	char *key_cpy;
+	char *value_cpy;
 
 	key_cpy = NULL;
-	key_cpy = ft_copy_key(env[0], key_cpy, '=');
-	printf("env[0] : %s\n", env[0]);
+	key_cpy = ft_copy_key(env[1], key_cpy, '=');
+
+	value_cpy = NULL;
+	value_cpy = ft_copy_value(env[1], value_cpy);
+
+	printf("env[0] : %s\n", env[1]);
 	printf("key_cpy : %s\n", key_cpy);
+	printf("value_cpy : %s\n", value_cpy);
 	return (0);
 }
