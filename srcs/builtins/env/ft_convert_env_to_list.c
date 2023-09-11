837f57d6dd27190404c48488f04bb2a1175d4b94
Basil ALBADER <baalbade@student.42.fr>
2023-09-11 10:01:29 +0200

    ft_convert_env_to_list.c

--- srcs/builtins/env/ft_convert_env_to_list.c
+++ srcs/builtins/env/ft_convert_env_to_list.c
@@ -12,46 +12,21 @@
 
 #include "minishell.h"
 
-t_env	*ft_create_new_env_node(int id)
-{
-	t_env	*new_node;
-
-	new_node = (t_env *)malloc(sizeof(t_env));
-	if (!new_node)
-		return (NULL);
-	new_node->id = id;
-	new_node->key = NULL;
-	new_node->value = NULL;
-	new_node->next = NULL;
-	return (new_node);
-}
-
-t_env	*ft_create_env_list(int env_len, t_env *env_lst)
+t_env	*ft_create_env_list(int env_len, t_env *env_lst, char **env)
 {
 	t_env	*head;
+	char	*key;
+	char	*value
 	int		i;
 
 	head = env_lst;
 	i = 0;
 	while (i < env_len)
 	{
-		env_lst->next = ft_create_new_env_node(i);
+		key = ft_copy_key(env[i], key, '=');
+		value = ft_copy_value(env[i], value);
+		env_lst->next = ft_create_new_env_node(i, key, value);
 		++i;
 	}
 	return (head);
 }
-
-/*
-t_env	*ft_assign_env_list(char **env, t_env *env_lst)
-{
-	(void)env;
-	(void)env_lst;
-	return (NULL);
-}
-
-t_env	*ft_convert_env_to_list(char **env)
-{
-	(void)env;
-	return (NULL);
-}
-*/
