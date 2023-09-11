1a35f17cfa091a84688557bd082ab261b85744fb
Basil ALBADER <baalbade@student.42.fr>
2023-09-11 09:43:13 +0200

    ft_copy_value.c

--- srcs/builtins/env/ft_copy_value.c
+++ srcs/builtins/env/ft_copy_value.c
@@ -12,14 +12,26 @@
 
 #include "minishell.h"
 
-char	*ft_copy_value(char *env, char *value_cpy, char stop)
+char	*ft_copy_value(char *env_elem, char *value_cpy)
 {
 	int	value_len;
 	int	i;
+	int	j;
 
-	if (!value)
+	if (!env_elem)
 		return (NULL);
-	i = 0;
-	while ()
-	
+	value_len = ft_get_value_len(env_elem);
+	i = ft_get_key_len(env_elem) + 1;
+	value_cpy = (char *)malloc(sizeof(char) * (value_len + 1));
+	if (!value_cpy)
+		return (NULL);
+	j = 0;
+	while (env_elem[i])
+	{
+		value_cpy[j] = env_elem[i];
+		++i;
+		++j;
+	}
+	value_cpy[j] = '\0';
+	return (value_cpy);
 }
