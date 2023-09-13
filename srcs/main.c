f86bf2bdd676b536a25e8c9f09e4b1f94aeeb9b4
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Dr6s7.clusters.42paris.fr>
2023-09-13 14:44:41 +0200

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 10:31:52 by baalbade          #+#    #+#             */
-/*   Updated: 2023/09/08 10:31:53 by baalbade         ###   ########.fr       */
+/*   Updated: 2023/09/13 14:44:32 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	t_env	*env_list;
+/*
 	int		env_len;
 
 	env_len = ft_get_env_len(env);
@@ -24,5 +25,6 @@ int main(int ac, char **av, char **env)
 	env_list = ft_convert_env_to_list(env_len, env_list, env);
 	ft_exec_env(env_list, 1);
 	ft_del_env(env_list);
+*/
 	return (0);
 }
