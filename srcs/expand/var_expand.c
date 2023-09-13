98c86a9a961f9ff278a429829d5d56ffd26b4842
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Dr6s7.clusters.42paris.fr>
2023-09-13 13:25:39 +0200

    var_expand

--- srcs/expand/var_expand.c
+++ srcs/expand/var_expand.c
@@ -6,34 +6,26 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:15:51 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/09/13 11:55:11 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/09/13 13:25:13 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
 
-char	*var_expand(char *str, char **varlst)
+char	*var_expand(char *str,  t_list *varlst)
 {
 	char	*new_str;
-	char	*var_buffer;
-	int		flag;
+	char	**split_str;
 	int		i;
 
-	flag = 0;
+	split_str = ft_split(str, '$');
+	if (!split_str)
+		return (NULL);
 	i = 0;
-	while (str[flag])
+	while (split_str[i])
 	{
-		while (str[flag] != '$' && ft_isspace(str[flag + 1]) && str[flag])
-			flag++;
-		new_str = ft_substr(str, i, flag - i);
-		i = flag;
-		while (!ft_isspace(str[flag]) && str[flag])
-			flag++; 
-		var_buffer = ft_substr(str, i, flag - i);
-		var_buffer = subst_var(var_buffer, varlst);
-		new_str = ft_strjoin_free(new_str, var_buffer);
-		free(var_buffer);
-		i = flag;
+		add_var_to_str(split_str, i, varlst);
+		
 	}
 	return (new_str);
 }
