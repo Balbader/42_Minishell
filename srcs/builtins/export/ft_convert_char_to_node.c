e3f896ac73ee4019035fc90e363af069bf332eff
Basil ALBADER <baalbade@student.42.fr>
2023-09-14 11:51:19 +0200

    WIP

--- srcs/builtins/export/ft_convert_char_to_node.c
+++ srcs/builtins/export/ft_convert_char_to_node.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
+t_env	*ft_convert_char_to_node(char *new_last)
+{
+	char	*key;
+	char	*value;
+	t_env	*new_node;
+
+	key = NULL;
+	value = NULL;
+	key = ft_copy_key(new_last, key, '=');
+	value = ft_copy_value(new_last, value);
+
+	new_node = ft_create_new_env_node(key, value);
+	return (new_node);
+}
