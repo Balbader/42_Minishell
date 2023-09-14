1501ae0a44b50c73033527f264ad4cb75dfc4e24
Basil ALBADER <baalbade@student.42.fr>
2023-09-14 10:58:04 +0200

    WIP

--- srcs/main.c
+++ srcs/main.c
@@ -21,12 +21,14 @@ int main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
-	t_env	*env_list;
+	t_env	*env_list = NULL;
 	int		env_len;
+	t_env	*new_lst = NULL;
 
 	env_len = ft_get_env_len(env);
-	env_list = NULL;
 	env_list = ft_convert_env_to_list(env_len, env_list, env);
+	new_lst = ft_exec_export("mimi=balou", env_list);
+	ft_exec_env(new_lst, 1);
 	ft_del_env(env_list);
 	return (0);
 }
