68f10477bf445414a96dc9a191243ad5817508b7
Basil ALBADER <baalbade@student.42.fr>
2023-09-14 09:48:04 +0200

    WIP

--- srcs/builtins/export/ft_exec_export.c
+++ srcs/builtins/export/ft_exec_export.c
@@ -12,13 +12,20 @@
 
 #include "minishell.h"
 
+t_env	*ft_convert_char_to_node(char *to_add)
+{
+}
+
 // t_env *ft_exec_export(t_data *data)
-t_env *ft_exec_export(char *to_add, t_env *env)
+t_env *ft_exec_export(t_env *to_add, t_env *env)
 {
 	t_env	*new_ls
 	t_env	*new_head;
 
 	new_lst = env;
 	new_head = new_lst;
-	return (new_lst);
+	while (new_lst)
+		new_lst = new_lst->next;
+	new_lst->next = to_add;
+	return (new_head);
 }
