d6c230938e0b4086fa88fa24fc140722f096abaf
Basil ALBADER <baalbade@student.42.fr>
2023-09-14 13:11:07 +0200

    ft_add_var_to_env.c

---
+++ srcs/builtins/export/ft_add_var_to_env.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_add_var_to_env.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 12:01:36 by baalbade          #+#    #+#             */
+/*   Updated: 2023/09/14 12:01:37 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+t_env	*ft_add_var_to_env(char *new_last, t_env *env)
+{
+	t_env	*new_lst;
+	t_env	*new_head;
+	t_env	*to_add;
+
+	new_lst = env;
+	new_head = new_lst;
+	while (new_lst->next != NULL)
+		new_lst = new_lst->next;
+	to_add = (t_env *)malloc(sizeof(t_env));
+	if (!to_add)
+		return  (NULL);
+	to_add = ft_convert_char_to_node(new_last);
+	new_lst->next = to_add;
+	return (new_head);
+}
