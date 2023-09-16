82e61e307d1750170cfb4cd7cb1a2fd5444e4423
Basil ALBADER <baalbade@student.42.fr>
2023-09-16 11:35:38 +0200

    WIP

--- srcs/builtins/env/ft_convert_env_to_list.c
+++ srcs/builtins/env/ft_convert_env_to_list.c
@@ -12,56 +12,31 @@
 
 #include "minishell.h"
 
-t_env	*ft_convert_env_to_list(t_env *env_lst, char **env)
+t_env	*ft_convert_env_to_list(int env_len, t_env *env_lst, char **env)
 {
 	t_env	*head;
+	char	*var;
 	char	*key;
 	char	*value;
-	int		env_len;
 	int		i;
 
-	env_len = ft_get_env_len(env);
+	var = NULL;
 	key = NULL;
 	value = NULL;
+	var = ft_copy_var(env[0], var);
 	key = ft_copy_key(env[0], key, '=');
 	value = ft_copy_value(env[0], value);
-	env_lst = ft_create_new_env_node(key, value);
+	env_lst = ft_create_new_env_node(var, key, value);
 	head = env_lst;
 	i = 1;
 	while (i <= env_len)
 	{
+		var = ft_copy_var(env[0], var);
 		key = ft_copy_key(env[i], key, '=');
 		value = ft_copy_value(env[i], value);
-		env_lst->next = ft_create_new_env_node(key, value);
+		env_lst->next = ft_create_new_env_node(var, key, value);
 		env_lst = env_lst->next;
 		++i;
 	}
 	return (head);
 }
-
-// t_env	*ft_convert_env_to_list(t_env *env_lst, char **env)
-// {
-// 	t_env	*head;
-// 	char	*key;
-// 	char	*value;
-// 	int		env_len;
-// 	int		i;
-
-// 	env_len = ft_get_env_len(env);
-// 	key = NULL;
-// 	value = NULL;
-// 	key = ft_copy_key(env[0], key, '=');
-// 	value = ft_copy_value(env[0], value);
-// 	env_lst = ft_create_new_env_node(key, value);
-// 	head = env_lst;
-// 	i = 1;
-// 	while (i <= env_len)
-// 	{
-// 		key = ft_copy_key(env[i], key, '=');
-// 		value = ft_copy_value(env[i], value);
-// 		env_lst->next = ft_create_new_env_node(key, value);
-// 		env_lst = env_lst->next;
-// 		++i;
-// 	}
-// 	return (head);
-// }
