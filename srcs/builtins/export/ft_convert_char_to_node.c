da5e6a81a6fb7d38ad2e3e6e5c2dc9aaa1d0094c
Basil ALBADER <baalbade@student.42.fr>
2023-09-18 16:56:20 +0200

    WIP

--- srcs/builtins/export/ft_convert_char_to_node.c
+++ srcs/builtins/export/ft_convert_char_to_node.c
@@ -35,7 +35,6 @@ t_env	*ft_convert_char_to_node(char *new_last)
 	var = ft_copy_var(new_last, var);
 	key = ft_copy_key(new_last, key, '=');
 	value = ft_copy_value(new_last, value);
-
 	new_node = ft_create_new_env_node(var, key, value);
 	return (new_node);
 }
