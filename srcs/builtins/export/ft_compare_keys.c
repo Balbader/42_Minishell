a90a470b7a110dca6e011c313c9e09081422dcfd
Basil ALBADER <baalbade@student.42.fr>
2023-09-23 10:18:38 +0200

    WIP

--- srcs/builtins/export/ft_compare_keys.c
+++ srcs/builtins/export/ft_compare_keys.c
@@ -12,17 +12,6 @@
 
 #include "minishell.h"
 
-int	ft_compare_keys(char *key, char *key_cpy)
+int	ft_compare_keys(char *env_key, char *new_key)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (key[i] == key_cpy[j] && key[i] && key_cpy[j])
-	{
-		++i;
-		++j;
-	}
-	return (key[i] - key_cpy[j]);
 }
