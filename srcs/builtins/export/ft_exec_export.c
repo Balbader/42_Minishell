d02c500808dbf6ffa92644e039437cf3b024a41d
Basil ALBADER <baalbade@student.42.fr>
2023-09-23 10:18:39 +0200

    WIP

--- srcs/builtins/export/ft_exec_export.c
+++ srcs/builtins/export/ft_exec_export.c
@@ -12,10 +12,18 @@
 
 #include "minishell.h"
 
-// t_env *ft_exec_export(t_data *data)
-t_env *ft_exec_export(char *new_last, t_env *env)
+/*
+ * 1. convert var to new_node (var, key, value)
+ * 2. compare new_node->key && env->key
+ * 3. if cpm(new_node->key && env->key) == true
+ * 		. replace env->key->value with new_node->value
+ * 4. if cpm(new_node->key && env->key) == false
+ * 		. add new_node to env
+*/
+
+t_env *ft_exec_export(char *var, t_env *env)
 {
-	(void)new_last;
+	(void)var;
 	(void)env;
 	return (NULL); 
 }
