e8d9b27afb858c2d324028ac7a796202bb6ed902
Basil ALBADER <baalbade@student.42.fr>
2023-09-24 12:31:28 +0200

    WIP

---
+++ srcs/builtins/env.bak/ft_export_many_args.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_export_many_args.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 09:53:09 by baalbade          #+#    #+#             */
+/*   Updated: 2023/09/18 09:53:10 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+ * this function will take the token linked list sent by parsing
+ * the first token (node) = the command to be executed will be by passed
+ * the remaining word (arg) of each token will be added to env
+ * only if all the variable are different.
+*/
+
+void	ft_export_many_args(t_token *token, t_env **env)
+{
+	t_env	*new_env;
+
+	new_env = *env;
+	token = token->next;
+	while (token)
+	{
+		ft_add_var_to_env(token->word, &new_env);
+		token = token->next;
+	}
+}
