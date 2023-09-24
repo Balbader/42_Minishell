373aa3daec3398a2f7e7c6da9e4b109813f6230e
Basil ALBADER <baalbade@student.42.fr>
2023-09-24 15:03:18 +0200

    WIP

--- srcs/builtins/cd/ft_replace_pwd.c
+++ srcs/builtins/cd/ft_replace_pwd.c
@@ -16,22 +16,22 @@ void	ft_replace_pwd(void)
 {
 	static const char	old_pwd[9] = "OLDPWD=\0";
 	static const char	cur_pwd[5] = "PWD=\0";
-	char				*pwd;
-	char				*o_pwd;
+	char				*wd;
+	char				*o_wd;
 	char				*tmp;
 
-	o_pwd = ft_find_value("PWD");
-	pwd = getcwd(NULL, 0);
-	if (!pwd)
+	o_wd = ft_find_value("PWD");
+	wd = getcwd(NULL, 0);
+	if (!wd)
 		return (g_error = 127, (void)error_msg("getcwd fail\n"));
-	tmp = pwd;
-	pwd = ft_strjoin(cur_pwd, pwd);
+	tmp = wd;
+	wd = ft_strjoin(cur_pwd, wd);
 	free(tmp);
-	o_pwd = ft_strjoin(old_pwd, o_pwd);
+	o_wd = ft_strjoin(old_pwd, o_wd);
 	ft_del_from_env("OLDPWD");
-	ft_add_to_env(o_pwd);
-	free(o_pwd);
+	ft_add_to_env(o_wd);
+	free(o_wd);
 	ft_del_from_env("PWD");
-	ft_add_to_env(pwd);
-	free(pwd);
+	ft_add_to_env(wd);
+	free(wd);
 }
