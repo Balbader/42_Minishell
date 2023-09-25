85bc717eba1d83a90a62d68eb6679a86209a36ab
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Dr11s17.clusters.42paris.fr>
2023-09-25 18:03:49 +0200

    function regrouping

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:49:15 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/09/25 16:50:39 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:52:06 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,66 @@ char	**convert_arg_to_tab(t_token *arg)
 		cmd_tab[i] = ft_strdup(arg->word);
 	cmd_tab[i] = NULL;
 	return (cmd_tab);
+}
+
+char	*copy_path(char **envp)
+{
+	int		i;
+	char	*path_copy;
+
+	i = 0;
+	while (envp[i] != 0)
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_copy = ft_strdup(envp[i] + 5);
+			return (path_copy);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	**create_path(char **envp)
+{
+	char	*strdup_path;
+	char	**split_path;
+	char	**comp_path;
+
+	if (envp == NULL)
+	{
+		ft_putstr_fd("envp error\n", 2);
+		exit(127);
+	}
+	strdup_path = copy_path(envp);
+	split_path = ft_split(strdup_path, ':');
+	comp_path = loop_joinf(split_path, "/");
+	free(strdup_path);
+	return (comp_path);
+}
+
+char	*init_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*individual_path;
+	t_list	*dump;
+	int		i;
+
+	paths = create_path(envp);
+	dump = ft_calloc(1, sizeof(t_list));
+	add_str_to_dump(paths, dump);
+	i = 0;
+	while (paths[i] != 0)
+	{
+		individual_path = ft_strjoin(paths[i], cmd);
+		if (access(individual_path, X_OK) == 0)
+		{
+			dump_del(dump);
+			return (individual_path);
+		}
+		dump_add(individual_path, dump);
+		i++;
+	}
+	dump_del(dump);
+	return (0);
 }
