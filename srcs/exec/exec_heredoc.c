d96ecac07dbd7331a5bf0acecb772197aa4171d8
Floriane TUERNAL-SABOTINOV <ftuernal@made-f0Dr10s18.clusters.42paris.fr>
2023-09-26 12:22:50 +0200

    redirection management

--- srcs/exec/exec_heredoc.c
+++ srcs/exec/exec_heredoc.c
@@ -1,26 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec.c                                             :+:      :+:    :+:   */
+/*   exec_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/22 11:40:06 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/09/26 11:21:51 by ftuernal         ###   ########.fr       */
+/*   Created: 2023/09/26 11:38:36 by ftuernal          #+#    #+#             */
+/*   Updated: 2023/09/26 12:22:33 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exec(t_data *in)
-{
-	in->cmd = NULL;
-	
-	if (exec_new_node_alloc(&cmd) == FAILURE)
-		return (FAILURE);
-	if (exec_new_node_alloc(&in->cmd) == FAILURE)
-		return (FAILURE);
-	if (exec_list_create(&in->cmd, expand_cmdline) == FAILURE)
-		return (FAILURE);
-	execute(in->cmd);
-}
+
