c68433efb8f6f82aed4090214a9e5e2f8f8a2f24
Floriane TUERNAL-SABOTINOV <ftuernal@bess-f2r6s4.clusters.42paris.fr>
2023-10-02 15:26:20 +0200

    fc

--- srcs/builtins/env/ft_get_env.c
+++ srcs/builtins/env/ft_get_env.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 11:20:15 by baalbade          #+#    #+#             */
-/*   Updated: 2023/09/29 14:08:51 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:26:02 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 t_env	**ft_get_env(char **env, char *to_add, char *to_del)
 {
-	static t_env	*list;
+	static t_env	*list = NULL;
 
 	if (env)
-	{
-		list = NULL;
 		list = ft_create_env_list(env);
-	}
 	else if (to_add)
 		ft_add_var_to_env(&list, to_add);
 	else if (to_del)
