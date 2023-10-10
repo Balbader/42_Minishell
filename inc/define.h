544a319e6dd599e89ab1fc6c59780b6c937db3e8
Basil ALBADER <baalbade@student.42.fr>
2023-10-10 14:08:27 +0200

    WIP

--- inc/define.h
+++ inc/define.h
@@ -16,15 +16,15 @@
 # define DEFINE_H
 
 # define ERROR_FILE "Error filename after redir -> function to do\n"
-# define ERROR_PARSING "Minishell : syntax error near unexpected token"
+# define ERROR_PARSING "$> bash : syntax error near unexpected token"
 # define ERROR_RED "No such file or directory\n"
 # define ERROR_EXP_START "export: "
 # define ERROR_EXP_END "': not a valid identifier\n"
 # define ERROR_CMD ": command not found\n"
 # define ERROR_ARG_CD "Wrong number of argument\n"
-# define ERROR_TO_MANY "Minishell: exit: too many arguments\n"
-# define ERROR_POINT "Minishell: .: filename argument required\n"
+# define ERROR_TO_MANY "$> bash: exit: too many arguments\n"
+# define ERROR_POINT "$> bash: .: filename argument required\n"
 # define ERROR_DOUBLE_POINT "..: command not found\n"
-# define ERROR_POINT_SLASH "Minishell: ./: Is a directory\n"
-# define ERROR_FORK "Minishell: fork() function error\n"
+# define ERROR_POINT_SLASH "$> bash: ./: Is a directory\n"
+# define ERROR_FORK "$> bash: fork() function error\n"
 #endif // !DEFINE_H
